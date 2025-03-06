5ba922ada6b77db326a98c35406f613485e42d7a
superUserT <rantshothabisomail@gmail.com>
2025-03-06 20:34:59 +0200

    added heading for statistics

--- part1/unicafe/src/App.tsx
+++ part1/unicafe/src/App.tsx
@@ -40,8 +40,49 @@ const Statistics = ({
   );
 };
 
+const All = ({
+  good,
+  neutral,
+  bad,
+}: {
+  good: number;
+  neutral: number;
+  bad: number;
+}) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <p>all: No feedback given</p>;
+  } else {
+    return <p>all {good + bad + neutral}</p>;
+  }
+};
+
+const Average = ({ good, bad }: { good: number; bad: number }) => {
+  if (good === 0 && bad === 0) {
+    return <p>Average: No feedback given</p>;
+  } else {
+    return <p>average: {(good - bad) / (good + bad)}</p>;
+  }
+};
+
+const Positive = ({
+  good,
+  bad,
+  neutral,
+}: {
+  good: number;
+  neutral: number;
+  bad: number;
+}) => {
+  if (good === 0) {
+    return <p>positive: No feedback given</p>;
+  } else {
+    return <p>positive: {(good / (good + bad + neutral)) * 100} %</p>;
+  }
+};
+
 const App = () => {
   const heading: string = "Give feedback";
+  const statistics: string = "Statistics";
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -52,7 +93,11 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Header text={statistics} />
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <All good={good} neutral={neutral} bad={bad} />
+      <Average good={good} bad={bad} />
+      <Positive good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
